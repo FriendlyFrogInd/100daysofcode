e8245ac264809835c2667e7a19789b513bef0a07
Neeraj <81550559+FriendlyFrogInd@users.noreply.github.com>
2024-07-18 13:47:55 +0530

    Update and rename main.py to 01 Print modifier.py

---
+++ Day 01 /01 Print modifier.py
@@ -0,0 +1,7 @@
+# Print Modifiers - 
+print("A 'single quote' inside a duoble quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can jsut \"escape\" the quote")
+
+
+

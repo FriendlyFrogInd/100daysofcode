864eb3201a9ca142d7335e4abab052f99308a238
Neeraj <81550559+FriendlyFrogInd@users.noreply.github.com>
2024-07-19 17:59:33 +0530

    Update main.py

--- Day 02/main.py
+++ Day 02/main.py
@@ -1,1 +1,48 @@
+#len function to get length
+eg. print(len("hello"))
 
+#data types 
+1. string - string of character 
+
+#suscript 
+to pull out number from string 
+eg. print("hello"[0])
+
+#integer data types
+eg. num = 4
+    num_2 = 5
+    print(num + num_2)
+
+#large integer 
+eg. 123_456_789 interpreted as 123456789 by python/computer
+
+#float -> for decimal 
+3.14159
+
+#Boolean -> true or false
+
+-----------------------------------------------
+Type error , type checking and type conversion 
+
+#type() -> to check data type
+
+str("put the variable here that we want to convert") -> convert the variable into string data type.
+
+-----------------------------------------------
+Mathematical operation :
+  +
+  -
+  * 
+  / -> when dividing we always get floating value 
+  ** -> raise the number to power 
+  eg . print(2 ** 2) -> 4
+
+  #PEMDAS rule for priority in mathematical operation 
+  -> parenteses
+  -> exponents
+  -> multiplication 
+  -> divisions
+  -> addition 
+  -> subtraction 
+  
+  
